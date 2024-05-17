67ed70770bde9868fb283ab6870294a0690a9119
Dimitar Angelov <funkamateur@gmail.com>
2024-05-17 23:49:53 +0200

    First working version of the AC29 firmware!

--- Examples/ASM/ac29fw.asm
+++ Examples/ASM/ac29fw.asm
@@ -9,65 +9,69 @@
 ; Compile:
 ; asm6809 -3 -S -v -o ac29fw.s19 ac29fw.asm
 
-		ORG $0000
+		 ORG     $1100
+D_CPU	RMB 1
+ACIA_C  EQU $A000
+ACIA_D  EQU $A001
+RAM_S   EQU $0000
+RAM_E   EQU $7FFF
 
-ACIA_C 	EQU $A000 				; ACIA control register address
-ACIA_D 	EQU $A001 				; ACIA data register address
+        ORG     $1500
+Start
+        LDX     #BootMsg
+        JSR     PrintMsg
+        LDX     #CopyMsg
+        JSR     PrintMsg
+		JSR		ChkCPU
+        JSR     PrintCPU
+Ending  SWI
+                                ; Check if 6309
+ChkCPU
+        LDA     #8              ; Assume 6809 at start
+        STA     D_CPU           ; Store 8 to indicate 6809
+        TFR     CC,DP           ; TFR CC,DP is valid only on 6309, illegal on 6809
+        BCS     Is6809          ; Branch if illegal instruction (6809)
 
-A_CPU3	EQU "6309"
-A_CPU8  EQU "6809"
-D_CPU	RMB 1					; equ "6309"
-D_SPD 	RMB 2					; equ "-.-"
-D_BPS	EQU "115200"
-D_ACIA	EQU "$A000"
+Is6309
+        LDA     #3
+        STA     D_CPU           ; Store 3 to indicate 6309
 
-		ORG		$1000
+Is6809
+EndChkCPU
+        RTS
 
-Start	
-ChkCPU 							; Check CPU type
-		PSHS    D               ; Save Reg-D
-		FDB     $1043           ; 6309 COMD instruction (COMA on 6809)
-		CMPB    1,S             ; not equal if 6309
-		BNE     Is6309          ; Branch to Is6309 if not equal
+PrintCPU
+        LDX     #CPU_MSG        ; Point X to the start of the "CPU Type:" message
+        JSR     PrintMsg        ; Print the "CPU Type:" message
+        LDX     #MSG_6809       ; Default to 6809 message
+        LDA     D_CPU           ; Load the CPU type value
+        CMPA    #3              ; Compare it to 3 (6309)
+        BEQ     Print6309       ; If 3, change pointer to 6309 message
+        BRA     PrintMsg        ; Jump to print message loop
 
-Is6809  
-		LDA		#33
-		STA 	D_CPU
-		BRA 	ChkCPU_End
+Print6309
+        LDX     #MSG_6309       ; Load message for 6309
 
-Is6309  
-        LDA 	#33
-        STA 	D_CPU
-	
-ChkCPU_End
-		PULS    D,PC
+PrintMsg
+        LDA    ,X+              ; Load a character from pointed message
+        BEQ     PrintDone       ; Check for end of message
+        STA     ACIA_D          ; Output character to ACIA
+        JSR     Delay           ; Introduce a delay
+        BRA     PrintMsg        ; Loop to print next character
+PrintDone
+        RTS                     ; Return from subroutine
 
-
-ChkSpeed						; Check the available memory. Zero out each checked cell.
-ChkSpeedEnd
-
-ChkMem							; Check the available memory. Zero out each checked cell.
-ChkMemEnd
-
-
-PrintM	ldx 	#BOOTMSG 			; Load address of the message
-MsgLoop    lda 	,x 					; Load the next character from the message
-        beq 	Done 				; If it's the null terminator, we're done
-        sta 	ACIA_D 				; Otherwise, send the character to ACIA
-        jsr 	Delay 				; Introduce a delay
-        leax 	1, x 				; Move to the next character in the message
-        bra 	MsgLoop 				; Repeat for the next character
-		
-Done    RTS 						; Stop execution
-
-Delay   LDA 	#100 				; Delay between character output 
+Delay
+        LDA     #100            ; Delay between character output 
 Delay_Loop
-        SUBA	#1					; Decrement the counter
-        BNE 	Delay_Loop 			; Repeat until the counter is zero
-        RTS 						; Return from subroutine
+        SUBA    #1              ; Decrement the counter
+        BNE     Delay_Loop      ; Repeat until the counter is zero
+        RTS                     ; Return from subroutine
 		
-BOOTMSG FCC 	"AC6309/AC29 rev.1, December 2023", $0A, $0D
-		FCC		"CPU: ", D_CPU , " at ", D_SPD, " MHz", $0A, $0D
-		FCC		"RAM: 32K $0000 - $7FFF", $0A, $0D
-		FCC		"ROM: 16K $C000 - $FFFF", $0A, $0D
-		FCC		"ACIA: ", D_BPS, " baud at ", D_ACIA, $0A, $0D, 0		
+
+CPU_MSG  FCC    "CPU: ",0
+RAM_MSG  FCC    "RAM: ",0
+MSG_6309 FCC    "Hitachi 6309", $0A, $0D, 0
+MSG_6809 FCC    "Motorola 6809", $0A, $0D, 0
+BootMsg  FCC    "AC6309/AC29, Rev. 1", $0A, $0D, 0
+CopyMsg  FCC    "(c) 2023 Dimitar Angelov", $0A, $0D, 0
